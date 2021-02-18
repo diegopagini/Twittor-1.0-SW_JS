3dc627e206d80556487ffcb5b10840364495696f
Diego Pagini <dq@qbkconsulting.com>
2021-02-18 08:30:36 -0300

    modificaciones para iphone x ++

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@
 //Imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 //Cache estatico.
@@ -48,6 +48,9 @@ self.addEventListener('activate', (event) => {
 	const response = caches.keys().then((keys) => {
 		keys.forEach((key) => {
 			if (key !== STATIC_CACHE && key.includes('static')) {
+				return caches.delete(key);
+			}
+			if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
 				return caches.delete(key);
 			}
 		});
