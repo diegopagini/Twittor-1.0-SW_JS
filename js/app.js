f84cc8e452912284aed3128cb9a28fe1c0f84288
Diego Pagini <dq@qbkconsulting.com>
2021-02-18 08:36:35 -0300

    test

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /** @format */
 var url = window.location.href;
 // var sWLocation = '/Twittor-SW_JS';
-var swLocation = '/twittor/sw.js';
+var sWLocation = '/twittor/sw.js';
 
 // Instalar SW.
 if (navigator.serviceWorker) {
